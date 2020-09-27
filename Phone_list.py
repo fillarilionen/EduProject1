86880f1e9b1fb9a624c3fb296fc96e0f0c2b03c4
Fedor <fillarilionen@gmail.com>
2020-09-27 15:24:56 +0300

    Много раз пропустил коммит на гит, т.к. были минорные изменения. В итоге они вылелись в доделанную прогу. Есть несколько багов, но править их нецелесообразно.

--- Phone_list.py
+++ Phone_list.py
@@ -9,24 +9,41 @@
 # - Удалить контакт
 # - Редактировать контакт
 # - Просмотреть список контактов
-
 # Реализовать проверку введенного номера телефона(может быть введен в формате +79681234567, 8(968)1234567), 8-968-595-31) и приводить номер при записи всегда к формату 8(968)595-31-78.
 # Номера сохраняются в csv-файл.
 # вывод в консоль.
 # В процессе написания использовать git
+# Файл, в котором хранятся данные(по умолчанию Data.csv) лежит в корне проекта, в папке PhoneBooks.
+
 import pandas as pd
 import numpy as np
 import collections
+import phonenumbers
 
 from collections import Counter
-# Файл, в котором хранятся данные(по умолчанию Data.csv) лежит в корне проекта, в папке PhoneBooks.
-# Пользователь при желании может создать другой файл. Реализовать возможность переключения между файлами. Мердж файлов(все, список выбранных).
-# Если файл Data.csv не найден, предложить другие файлы на выбор, если их нет то создать новый(Data.csv).
+
+def Number_Check(Number) :
+    if Number[0] != "+" :
+        Number = Number.replace("8","", 1)
+        Country_code = "+7"
+        Number = Country_code + Number
+    if len(Number) == 12 :
+        Number = phonenumbers.parse(Number, None)
+        if phonenumbers.is_valid_number(Number):
+            Number = phonenumbers.format_number(Number, phonenumbers.PhoneNumberFormat.NATIONAL)
+            return (Number,True)
+    return (Number,False)
+
 def New_Contact(phone_number_list) :
     print('Введите имя')
     Name = input()
-    print('Введите номер')
-    Number = input()
+    while True:
+        print('Введите номер')
+        Number = input()
+        New_number = Number_Check(Number)
+        if New_number[1] == True :
+            Number = New_number[0]
+            break
     print('Введите Должность') #сделать опционально
     Position = input()
     print('Введите Организацию') #сделать опционально
@@ -47,8 +64,13 @@ def New_Contact(phone_number_list) :
 def Contact_editing(phone_number_list,contact_index) :
     print('Введите имя')
     Name = input()
-    print('Введите номер')
-    Number = input()
+    while True:
+        print('Введите номер')
+        Number = input()
+        New_number = Number_Check(Number)
+        if New_number[1] == True :
+            Number = New_number[0]
+            break
     print('Введите Должность') #сделать опционально
     Position = input()
     print('Введите Организацию') #сделать опционально
@@ -77,30 +99,24 @@ def Edit_and_delete(contact_index,phone_number_list) :
 
 def Show_number_list(phone_number_list) :
     print(phone_number_list)
-    print('Введите номер нужного контакта')
+    print('Введите номер контакта, который хотите отредактировать')
     Edit_and_delete(int(input()),phone_number_list)
 
 def Main() :
     phone_number_list = pd.read_csv('PhoneBooks/Data.csv')
     print('Выберите действие\n', '0 - Выйти\n', '1 - создать новый контакт\n', '2 - Просмотреть список контактов\n')
     while True:
-        zaebavshiy_menya_input = int(input())
-        if zaebavshiy_menya_input == 0 :
+        Menu_input = int(input())
+        if Menu_input == 0 :
             break
-        elif zaebavshiy_menya_input == 1 :
+        elif Menu_input == 1 :
             New_Contact(phone_number_list)
             Main()
-        elif zaebavshiy_menya_input == 2 :
+        elif Menu_input == 2 :
             Show_number_list(phone_number_list)
             Main()
         else:
-            print('ведите правильное значение')
+            print('Ведите правильное значение')
             Main()
 
-
 Main()
-
-
-
-
-
